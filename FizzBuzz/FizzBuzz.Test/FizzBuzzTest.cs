ce9295bda6cfcd1c4569040bdfe0f159c1b57260
Roman <1815131bill@gmail.com>
2022-02-06 02:48:56 +0300

    Create Check for division by 3

--- FizzBuzz/FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzz.Test/FizzBuzzTest.cs
@@ -6,13 +6,20 @@ namespace FizzBuzzKata.Test
     [TestFixture]
     public class FizzBuzzTest
     {
+        private const string Expected = "1\n2\nFizz\n4\n5\nFizz\n7\n8\nFizz\n10\n11\nFizz\n13\n14\nFizz" +
+                                        "\n16\n17\nFizz\n19\n20\nFizz\n22\n23\nFizz\n25\n26\nFizz\n28\n29" +
+                                        "\nFizz\n31\n32\nFizz\n34\n35\nFizz\n37\n38\nFizz\n40\n41\nFizz\n43" +
+                                        "\n44\nFizz\n46\n47\nFizz\n49\n50\nFizz\n52\n53\nFizz\n55\n56\nFizz" +
+                                        "\n58\n59\nFizz\n61\n62\nFizz\n64\n65\nFizz\n67\n68\nFizz\n70\n71\nFizz" +
+                                        "\n73\n74\nFizz\n76\n77\nFizz\n79\n80\nFizz\n82\n83\nFizz\n85\n86\nFizz" +
+                                        "\n88\n89\nFizz\n91\n92\nFizz\n94\n95\nFizz\n97\n98\nFizz\n100\n"; 
+        
         [Test]
-        public void Print_numbers_from_1_to_100()
+        public void Check_for_Divisible_3()
         {
-            var result = FizzBuzzClass.PrintHundred();
-            var expected = string.Join("\n", Enumerable.Range(1, 100)) + '\n';
+            var result = FizzBuzzClass.FizzBuzz();
             
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(Expected, result);
         }
     }
 }
--- FizzBuzz/FizzBuzz/FizzBuzzClass.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzClass.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Text;
 
 namespace FizzBuzzKata
 {
     public static class FizzBuzzClass
     {
-        public static string PrintHundred()
+        public static string FizzBuzz()
         {
             var hundred = new StringBuilder();
             for (var i = 1; i <= 100; i++)
+            {
                 hundred.Append(i).Append('\n');
+            }
             
             return hundred.ToString();
         }
